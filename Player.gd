4d539607134661ff07d49b96607dcdfd0b1fc0c3
LuizzDutra <85079817+LuizzDutra@users.noreply.github.com>
2022-08-07 14:17:11 -0300

    arquivos protótipo de customização

---
+++ Player.gd
@@ -0,0 +1,53 @@
+extends KinematicBody2D
+
+
+onready var animationPlayer = $AnimationPlayer
+onready var bodyParts = $BodyParts
+
+func _ready():
+	bodyParts.get_node("Head").self_modulate = Color(1, 1, 1)
+
+
+const head_qnt = 11
+const body_qnt = 8
+const leg_qnt = 3
+
+var head_select = 0 setget changeHead
+var body_select = 0 setget changeBody
+var leg_select = 0 setget changeLeg
+
+
+
+func _process(delta):
+	animationPlayer.play("IdleRight")
+
+func changeHead(n):
+	head_select = n
+	bodyParts.get_node("Head").frame_coords.y = head_select % head_qnt + 1
+	
+func changeBody(n):
+	body_select = n
+	bodyParts.get_node("Body").frame_coords.y = body_select % body_qnt + 1
+
+func changeLeg(n):
+	leg_select = n
+	bodyParts.get_node("Legs").frame_coords.y = leg_select % leg_qnt + 1
+
+func randomizedSelection():
+	changeHead(int(rand_range(0, head_qnt)))
+	changeBody(int(rand_range(0, body_qnt)))
+	changeLeg(int(rand_range(0, leg_qnt)))
+	
+
+func _on_Head_pressed():
+	changeHead(head_select+1)
+	
+func _on_Body_pressed():
+	changeBody(body_select+1)
+
+func _on_Legs_pressed():
+	changeLeg(leg_select+1)
+
+
+func _on_Random_pressed():
+	randomizedSelection()
