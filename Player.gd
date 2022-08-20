862c69eb53479b163f8d0776a56f2d5fb9c861e3
LuizzDutra <85079817+LuizzDutra@users.noreply.github.com>
2022-08-20 20:46:42 -0300

    Add files via upload

--- Player.gd
+++ Player.gd
@@ -3,9 +3,12 @@ extends KinematicBody2D
 
 onready var animationPlayer = $AnimationPlayer
 onready var bodyParts = $BodyParts
+onready var buttons = $Buttons
+onready var customizationState = true setget toggleCusState
 
 func _ready():
-	changeHeadModulate(Color(1,1,1))
+	buttons.visible = customizationState
+	changeBodyModulate(Color(1,1,1))
 
 
 const head_qnt = 11
@@ -17,12 +20,16 @@ var body_select = 0 setget changeBody
 var leg_select = 0 setget changeLeg
 
 var head_modulate = Color(1, 1, 1) setget changeHeadModulate
+var body_modulate = Color(1, 1, 1) setget changeBodyModulate
 
 var animation_arr = ["IdleRight", "WalkRight"]
 var cur_anim = 0
 
 
 func _process(delta):
+	if Input.is_action_just_pressed("tgg_cust"):
+		toggleCusState(0)
+	
 	animationPlayer.play(animation_arr[cur_anim])
 
 func changeHead(n):
@@ -46,6 +53,21 @@ func changeHeadModulate(n: Color):
 	head_modulate = n
 	bodyParts.get_node("Head").modulate = n
 
+func changeBodyModulate(n: Color):
+	body_modulate = n
+	bodyParts.get_node("Body").modulate = n
+	
+func toggleCusState(n):
+	if typeof(n) != TYPE_BOOL:
+		customizationState = !customizationState
+	else:
+		customizationState = n
+		
+	if customizationState:
+		buttons.visible = true
+	else:
+		buttons.visible = false
+
 func _on_Head_pressed():
 	changeHead(head_select+1)
 	
@@ -55,10 +77,18 @@ func _on_Body_pressed():
 func _on_Legs_pressed():
 	changeLeg(leg_select+1)
 
-
 func _on_Random_pressed():
 	randomizedSelection()
 
 func _on_Animation_pressed():
 	cur_anim += 1
 	cur_anim = cur_anim % len(animation_arr)
+
+func _on_HeadColorChanger2_color_changed(color):
+	changeHeadModulate(color)
+	
+
+func _on_BodyColorChanger_color_changed(color):
+	changeBodyModulate(color)
+
+
